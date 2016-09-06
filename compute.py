e38a6340ac3ed415e33c440746936476e2621118
devinjdawson <devinjdawson@me.com>
2016-09-06 19:21:14 -0400

    Update compute.py
    Changed coinmarketcap-nexuist request

--- compute.py
+++ compute.py
@@ -6,11 +6,11 @@ r = requests.get('https://etherchain.org/api/basic_stats').json()
 def avg(l): return sum([i for i in l])/float(len(l))
 blockTimeAvg = avg([int(b['blockTime']) for b in r['data']['blocks']])
 difficultyAvg = avg([int(b['difficulty']) for b in r['data']['blocks']])
-r = requests.get('http://coinmarketcap-nexuist.rhcloud.com/api/eth').json()
+r = requests.get('http://coinmarketcap-nexuist.rhcloud.com/api/xmr').json()
 data = {
     'blockTime': blockTimeAvg,
     'difficulty': difficultyAvg,
     'priceUsd': float(r['price']['usd']),
     'lastUpdate': time.time(),
 }
-file(sys.argv[1], 'w').write('ethereumStats = ' + json.dumps(data) + ';')
+file(sys.argv[1], 'w').write('moneroStats = ' + json.dumps(data) + ';')
