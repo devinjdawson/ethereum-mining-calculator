183d96e340ce7fbde9a62c33435d526e4b2cf7a7
devinjdawson <devinjdawson@me.com>
2016-09-07 01:38:32 -0400

    Add files via upload

--- compute.py
+++ compute.py
@@ -2,15 +2,14 @@ import requests
 import json
 import sys
 import time
-r = requests.get('https://etherchain.org/api/basic_stats').json()
+r = requests.get('http://moneroblocks.info/api/get_stats/').json()
 def avg(l): return sum([i for i in l])/float(len(l))
-blockTimeAvg = avg([int(b['blockTime']) for b in r['data']['blocks']])
 difficultyAvg = avg([int(b['difficulty']) for b in r['data']['blocks']])
-r = requests.get('http://coinmarketcap-nexuist.rhcloud.com/api/xmr').json()
+r = requests.get('https://www.cryptocompare.com/api/data/price?fsym=XMR&tsyms=USD"').json()
 data = {
-    'blockTime': blockTimeAvg,
-    'difficulty': difficultyAvg,
-    'priceUsd': float(r['price']['usd']),
+    'blockTime': 120,
+    'difficulty': 'difficulty',
+    'priceUsd': 'Data.Price',
     'lastUpdate': time.time(),
 }
 file(sys.argv[1], 'w').write('moneroStats = ' + json.dumps(data) + ';')
